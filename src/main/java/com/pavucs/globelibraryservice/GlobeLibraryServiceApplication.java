6a21fb6c64767ba8fbde3c57193356a4961f5a6f
Pavucs Manokar <smpavucsan@gmail.com>
2022-11-09 22:27:19 +0530

    war config added

--- src/main/java/com/pavucs/globelibraryservice/GlobeLibraryServiceApplication.java
+++ src/main/java/com/pavucs/globelibraryservice/GlobeLibraryServiceApplication.java
@@ -2,11 +2,12 @@ package com.pavucs.globelibraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class GlobeLibraryServiceApplication {
+public class GlobeLibraryServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GlobeLibraryServiceApplication.class, args);
